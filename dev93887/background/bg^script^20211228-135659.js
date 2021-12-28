34f56a432ad66e162708ac78ca471ccfe39ab77d
Ganga Prasad Pattnaik <ganga2404@gmail.com>
2021-12-28 13:59:41 +1100

    bgscript commit

---
+++ dev93887/background/bg^script^20211228-135659.js
@@ -0,0 +1,3 @@
+// sn-scriptsync - Received from background script tab via SN Utils. (delete file after usage.)
+
+gs.log("this is a test")////
---
+++ dev93887/background/bg^script^20211228-135842.js
@@ -0,0 +1,5 @@
+// sn-scriptsync - Received from background script tab via SN Utils. (delete file after usage.)
+
+// sn-scriptsync - Received from background script tab via SN Utils. (delete file after usage.)
+
+gs.log("this is a test")////new line for new bgscript
